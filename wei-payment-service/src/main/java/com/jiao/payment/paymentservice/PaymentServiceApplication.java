b45c8a452ba7420b70557eacb02fd516a5d15806
jiaorongjin <2935996123@qq.com>
2025-02-10 00:43:05 +0800

    新建支付模块

---
+++ wei-payment-service/src/main/java/com/jiao/payment/paymentservice/PaymentServiceApplication.java
@@ -0,0 +1,16 @@
+package com.jiao.payment.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 29359
+ */
+@SpringBootApplication
+public class PaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+
+}
